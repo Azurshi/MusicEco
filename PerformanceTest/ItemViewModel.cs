c8dda10f976734a75c5884ac637c4db9df370faa
Azurshi <vdanh392004@gmail.com>
2026-08-13 23:57:22 +0700

    Remove test code

--- PerformanceTest/ItemViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PerformanceTest;
-
-public class ItemViewModel {
-    public string Text { get; init; }
-    public ItemViewModel(string text) {
-        this.Text = text;
-    }
-}
--- PerformanceTest/Platforms/iOS/AppDelegate.cs
+++
@@ -1,8 +0,0 @@
-﻿using Foundation;
-
-namespace PerformanceTest;
-
-[Register("AppDelegate")]
-public class AppDelegate: MauiUIApplicationDelegate {
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
